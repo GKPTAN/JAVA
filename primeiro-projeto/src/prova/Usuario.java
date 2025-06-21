0ab95caec6275185420b8bf50383f3ff431ed596
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2025-06-21 18:09:21 -0300

    atualização

---
+++ primeiro-projeto/src/prova/Usuario.java
@@ -0,0 +1,15 @@
+package prova;
+
+public class Usuario {
+    String nome;
+    int idade;
+
+    public Usuario(String n, int i) {
+        nome = n;
+        idade = i;
+    }
+
+    public static void main(String[] args) {
+        Usuario u = new Usuario("João", 25);
+    }
+}
